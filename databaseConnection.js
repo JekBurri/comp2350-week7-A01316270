a30377e02692938b8c1a4cb796ffeb9c4e1ba52b
Jack Boeri <53635108+JekBurri@users.noreply.github.com>
2023-02-27 09:33:01 -0800

    first commit

---
+++ databaseConnection.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql2/promise');
+
+const is_qoddi = process.env.IS_QODDI || false;
+
+const dbConfigQoddi = {
+	host: "sql.freedb.tech",
+	user: "freedb_2350main",
+	password: "&G3AbYA*auMSvZn",
+	database: "freedb_comp2350-week2-A01316270",
+	multipleStatements: false,
+	namedPlaceholders: true
+};
+
+const dbConfigLocal = {
+	host: "localhost",
+	user: "root",
+	password: "Password",
+	database: "database1",
+	multipleStatements: false,
+	namedPlaceholders: true
+};
+
+if (is_qoddi) {
+	var database = mysql.createPool(dbConfigQoddi);
+}
+else {
+	var database = mysql.createPool(dbConfigLocal);
+}
+
+module.exports = database;
+		
